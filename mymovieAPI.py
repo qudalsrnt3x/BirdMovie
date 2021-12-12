cb35ce06114a8636d1b0a527a09623ceb769dfe6
yejiseo0725 <yejiseo0725@gmail.com>
2021-12-13 02:55:59 +0900

    post로 영화 검색 후 코멘트 붙여서 db에 저장
    my-movie.html : makeReview() 작성
    mymovieAPI.py : listing() 작성

--- mymovieAPI.py
+++ mymovieAPI.py
@@ -1,6 +1,9 @@
 import requests
 from flask import Blueprint, request, jsonify
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 mymovieAPI = Blueprint('mymovie', __name__)
 
@@ -8,6 +11,8 @@ mymovieAPI = Blueprint('mymovie', __name__)
 @mymovieAPI.route('/my-movie', methods=['POST'])
 def listing():
     title = request.form['movie_title']
+    comment = request.form['movie_comment']
+
     url = 'https://movie.naver.com/movie/search/result.naver?query=' + title
 
     headers = {
@@ -17,8 +22,18 @@ def listing():
 
     soup = BeautifulSoup(data.text, 'html.parser')
 
-    image = soup.select_one('#old_content > ul:nth-child(4) > li:nth-child(1) > p > a > img')
+    image = soup.select_one('#old_content > ul:nth-child(4) > li:nth-child(1) > p > a > img').get('src')
     title = soup.select_one('#old_content > ul:nth-child(4) > li:nth-child(1) > dl > dt > a').text
-    print(image, title)
+    inform = soup.select_one('#old_content > ul:nth-child(4) > li:nth-child(1) > dl > dd:nth-child(3)').text
+    director_actor = soup.select_one('#old_content > ul:nth-child(4) > li:nth-child(1) > dl > dd:nth-child(4)').text
+
+    doc = {
+        'image':image,
+        'title':title,
+        'inform':inform,
+        'director_actor':director_actor,
+        'comment':comment
+    }
+    db.reviewdb.insert_one(doc)
 
     return jsonify({'msg':'GET 연결되었습니다!'})
